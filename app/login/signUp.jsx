2d52960464bdfb697774ae3ec055d01ff7bc52a0
Göksu Ayaz <goksuayaz79@gmail.com>
2025-01-02 13:37:14 +0300

    Sign Up Screen

--- app/login/signUp.jsx
+++ app/login/signUp.jsx
@@ -1,18 +1,20 @@
-import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native'
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native'
 import React from 'react'
 import Colors from '../../constant/Colors'
-import { useRouter } from 'expo-router'
+import { useRouter } from 'expo-router';
 
-
-export default function SignIn() {
+export default function SignUp() {
 
     const router = useRouter();
 
     return (
         <View style={{ padding: 25 }}>
-            <Text style={styles.textHeader}>Let's Sign You In</Text>
-            <Text style={styles.subText}>Welcome Back </Text>
-            <Text style={styles.subText}>You have been missed!</Text>
+            <Text style={styles.textHeader}>Create New Account</Text>
+
+            <View style={{ marginTop: 25 }}>
+                <Text> Full Name</Text>
+                <TextInput placeholder='Full Name' style={styles.textInput} />
+            </View>
 
             <View style={{ marginTop: 25 }}>
                 <Text> Email</Text>
@@ -30,19 +32,17 @@ export default function SignIn() {
                 <Text style={{
                     fontSize: 17, color: 'white',
                     textAlign: 'center'
-                }}> Login </Text>
-            </TouchableOpacity>
-
-
-            <TouchableOpacity style={styles.buttonCreate}
-                onPress={() => router.push('login/signUp')} >
-                <Text style={{
-                    fontSize: 17, color: Colors.PRIMARY,
-                    textAlign: 'center'
                 }}> Create Account </Text>
             </TouchableOpacity>
 
 
+            <TouchableOpacity style={styles.buttonCreate}
+                onPress={() => router.push('login/signIn')} >
+                <Text style={{
+                    fontSize: 17, color: Colors.PRIMARY,
+                    textAlign: 'center'
+                }}> Already account? Sign In </Text>
+            </TouchableOpacity>
         </View>
     )
 }
@@ -55,12 +55,6 @@ const styles = StyleSheet.create({
         marginTop: 15
     },
 
-    subText: {
-        fontSize: 30,
-        fontWeight: 'bold',
-        marginTop: 10,
-        color: Colors.GRAY
-    },
     textInput: {
         padding: 10,
         borderWidth: 1,
@@ -87,4 +81,4 @@ const styles = StyleSheet.create({
         borderColor: Colors.PRIMARY
 
     }
-})
+}) 
